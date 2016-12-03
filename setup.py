11fb8639e978968f78a7531770f9f3a5945607b8
Robert Roose <rroose@quid.com>
2016-12-03 10:56:02 -0800

    Removing requirements.txt

--- setup.py
+++ setup.py
@@ -9,24 +9,15 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-requirements_txt = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "requirements.txt"
-)
-requires = []
-
-
-with open(requirements_txt) as requirements:
-    requires = requirements.read().split()
-
-
 setup(
     name="actions",
-    version="1.0.0",
+    version="1.0.1",
     description="Declarative actions",
     author="Robert Roose",
     author_email="robert@shivercube.com",
     url="https://bitbucket.org/summatix/actions",
     packages=find_packages(),
-    install_requires=requires
+    install_requires=[
+        "bunch==1.0.1"
+    ]
 )
