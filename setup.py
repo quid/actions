8100afd5c5ecc8290feb6944ae0f6475c6fd50cd
Robert Roose <rroose@quid.com>
2016-12-08 15:18:06 -0800

    Transferring ownership to Quid

--- setup.py
+++ setup.py
@@ -9,13 +9,22 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert("README.md", "rst")
+
+except (IOError, ImportError):
+    long_description = open("README.md").read()
+
+
 setup(
     name="actions",
-    version="1.0.1",
+    version="1.0.2",
     description="Declarative actions",
-    author="Robert Roose",
-    author_email="robert@shivercube.com",
-    url="https://bitbucket.org/summatix/actions",
+    long_description=long_description,
+    author="Quid Inc.",
+    author_email="ops@quid.com",
+    url="https://github.com/quid/actions",
     packages=find_packages(),
     install_requires=[
         "bunch==1.0.1"
